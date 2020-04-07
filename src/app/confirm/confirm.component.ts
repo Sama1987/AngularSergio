6359faac974ab3b1e908c6ae93374d0b5d2cd605
Sergio Samaniego Abaunz <sergiosama.87@gmail.com>
2020-04-07 18:14:48 +0200

    Más cambios

--- src/app/confirm/confirm.component.ts
+++ src/app/confirm/confirm.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class ConfirmComponent implements OnInit {
 
   isDisabled: boolean;
+  showModal: boolean;
 
   constructor() {
     this.isDisabled = true;
